74cb55eb03f9d397bddd9f5ded57a39374356ed2
Cyano Hao <c@cyano.cn>
2019-02-02 00:00:12 +0800

    prepare for multiple charset

--- README.md
+++ README.md
@@ -1,19 +1,43 @@
-# Android Chinese (Simplified) Font Completion Generator
+# Android CJK Font Completion Generator
 
-Build Chinese (Simplified) Font Completion pack for Android N and O.
+Build Roboto, Roboto Condensed, and Noto Serif with CJK support.
 
-## How to Build
++ [Full](full/): simply merge Roboto / Noto Serif with Noto CJK SC. Used by [Chinese (Simplified) Font Completion](https://github.com/Magisk-Modules-Repo/chinesesimplified-font-completion) module.
++ [GB18030](gb18030/): share glyphs between Roman and Italic.
+
+<!-- + [GBK](gbk/): share glyphs among Roman, Italic, and Condensed Roman; Condensed Italic not available.
++ [通用规范汉字](通规/): share glyphs among Roman, Italic, Condensed Roman, and Condensed Italic. NOT RECOMMANDED, just for fun. -->
+
+## Build for Chinese (Simplified)
 
 Install [AFDKO](https://github.com/adobe-type-tools/afdko), [otfcc](https://github.com/caryll/otfcc) and [otfcc-c2q](https://github.com/caryll/otfcc-cubic2quad).
 
 Download Noto Sans CJK SC Thin/Light/Regular/Medium/Bold/Black (6 files), Noto Serif CJK SC Regular/Bold (2 files), Roboto Thin/Light/Regular/Medium/Bold/Black Roman/Italic (6 × 2 = 12 files), Roboto Condensed Light/Regular/Medium/Bold Roman/Italic (4 × 2 = 8 files), and Noto Serif Regular/Bold Roman/Italic (2 × 2 = 4 files).
 
-Put the 32 files mentioned above to `src/`, then
+Put these 32 files to `<charset>/src/`, then
 
 ```bash
+cd <charset>/
 make -j<threads>
 ```
 
 Note: 10 GiB memory required when running with `-j4`.
 
-Output can be found in `out/`.
+Output can be found in `<charset>/out/`.
+
+## Build for Chinese (Traditional), Japanese, and Korean
+
++ Full: can be built for Chinese (Traditional), Japanese, and Korean.
++ GB18030: can be built for Chinese (Traditional) and Japanese.
+
+<!-- + GBK: can be built for Chinese (Traditional) and Japanese.
++ 通用规范汉字: Chinese (Simplified) only, would be a disaster if build for other languages. -->
+
+To build, run
+
++ `sed -i 's/CJKsc/CJKtc/g' Makefile` for Chinese (Traditional, Taiwan)
++ `sed -i 's/SansCJKsc/SansCJKhk/g;s/SerifCJKsc/SerifCJKtw/g' Makefile` for Chinese (Traditional, Hong Kong) (quite different, for Noto Serif CJK has no HK variant)
++ `sed -i 's/CJKsc/CJKjp/g' Makefile` for Japanese
++ `sed -i 's/CJKsc/CJKkr/g' Makefile` for Korean
+
+before `make`ing.
--- full/configure.sh
+++ full/configure.sh
@@ -16,14 +16,14 @@ for weight in Thin Light Regular Medium Bold Black ThinItalic LightItalic Italic
 	cjkbase=NotoSansCJKsc-$weight
 	cat >>Makefile <<EOF
 out/$file: out/$basename.otd cjk/$cjkbase.otd
-	python merge.py out/$basename.otd cjk/$cjkbase.otd
+	PYTHONPATH=.. python merge.py out/$basename.otd cjk/$cjkbase.otd
 	otfccbuild out/$basename.otd -O3 -o out/$file
-	ttx -t 'head' -o $basename.ttx src/$file
+	ttx -t 'head' -o $basename.ttx ../src/$file
 	ttx -m out/$file -o out/$file $basename.ttx
 	rm $basename.ttx
-out/$basename.otd: src/$file
+out/$basename.otd: ../src/$file
 	mkdir -p out/
-	otfccdump src/$file --pretty --no-bom | sed 's|Copyright 2011 Google Inc. All Rights Reserved.|Portions Copyright 2011 Google Inc. Portions Copyright © 2014, 2015, 2018 Adobe Systems Incorporated (http://www.adobe.com/).|;s/Licensed under the Apache License, Version 2.0/This Font Software is licensed under the SIL Open Font License, Version 1.1./;s|http://www.apache.org/licenses/LICENSE-2.0|http://scripts.sil.org/OFL|' >out/$basename.otd
+	otfccdump ../src/$file --pretty --no-bom | sed 's|Copyright 2011 Google Inc. All Rights Reserved.|Portions Copyright 2011 Google Inc. Portions Copyright © 2014, 2015, 2018 Adobe Systems Incorporated (http://www.adobe.com/).|;s/Licensed under the Apache License, Version 2.0/This Font Software is licensed under the SIL Open Font License, Version 1.1./;s|http://www.apache.org/licenses/LICENSE-2.0|http://scripts.sil.org/OFL|' >out/$basename.otd
 EOF
 done
 for weight in Light Regular Medium Bold LightItalic Italic MediumItalic BoldItalic; do
@@ -32,14 +32,14 @@ for weight in Light Regular Medium Bold LightItalic Italic MediumItalic BoldItal
 	cjkbase=NotoSansCJKscCondensed-$weight
 	cat >>Makefile <<EOF
 out/$file: out/$basename.otd cjk/$cjkbase.otd
-	python merge.py out/$basename.otd cjk/$cjkbase.otd
+	PYTHONPATH=.. python merge.py out/$basename.otd cjk/$cjkbase.otd
 	otfccbuild out/$basename.otd -O3 -o out/$file
-	ttx -t 'head' -o $basename.ttx src/$file
+	ttx -t 'head' -o $basename.ttx ../src/$file
 	ttx -m out/$file -o out/$file $basename.ttx
 	rm $basename.ttx
-out/$basename.otd: src/$file
+out/$basename.otd: ../src/$file
 	mkdir -p out/
-	otfccdump src/$file --pretty --no-bom | sed 's|Copyright 2011 Google Inc. All Rights Reserved.|Portions Copyright 2011 Google Inc. Portions Copyright © 2014, 2015, 2018 Adobe Systems Incorporated (http://www.adobe.com/).|;s/Licensed under the Apache License, Version 2.0/This Font Software is licensed under the SIL Open Font License, Version 1.1./;s|http://www.apache.org/licenses/LICENSE-2.0|http://scripts.sil.org/OFL|' >out/$basename.otd
+	otfccdump ../src/$file --pretty --no-bom | sed 's|Copyright 2011 Google Inc. All Rights Reserved.|Portions Copyright 2011 Google Inc. Portions Copyright © 2014, 2015, 2018 Adobe Systems Incorporated (http://www.adobe.com/).|;s/Licensed under the Apache License, Version 2.0/This Font Software is licensed under the SIL Open Font License, Version 1.1./;s|http://www.apache.org/licenses/LICENSE-2.0|http://scripts.sil.org/OFL|' >out/$basename.otd
 EOF
 done
 for weight in Regular Bold Italic BoldItalic; do
@@ -48,14 +48,14 @@ for weight in Regular Bold Italic BoldItalic; do
 	cjkbase=NotoSerifCJKsc-$weight
 	cat >>Makefile <<EOF
 out/$file: out/$basename.otd cjk/$cjkbase.otd
-	python merge.py out/$basename.otd cjk/$cjkbase.otd
+	PYTHONPATH=.. python merge.py out/$basename.otd cjk/$cjkbase.otd
 	otfccbuild out/$basename.otd -O3 -o out/$file
-	ttx -t 'head' -o $basename.ttx src/$file
+	ttx -t 'head' -o $basename.ttx ../src/$file
 	ttx -m out/$file -o out/$file $basename.ttx
 	rm $basename.ttx
-out/$basename.otd: src/$file
+out/$basename.otd: ../src/$file
 	mkdir -p out/
-	otfccdump src/$file --pretty --no-bom | sed 's|Copyright 2015 Google Inc. All Rights Reserved.|Portions Copyright 2015 Google Inc. Portions Copyright © 2017 Adobe Systems Incorporated (http://www.adobe.com/).|' >out/$basename.otd
+	otfccdump ../src/$file --pretty --no-bom | sed 's|Copyright 2015 Google Inc. All Rights Reserved.|Portions Copyright 2015 Google Inc. Portions Copyright © 2017 Adobe Systems Incorporated (http://www.adobe.com/).|' >out/$basename.otd
 EOF
 done
 
@@ -64,14 +64,14 @@ for file in NotoSansCJKsc-{Thin,Light,Regular,Medium,Bold,Black}.otd NotoSansCJK
 	italic=$(echo $file | sed 's/.otd/Italic.otd/;s/Regular//')
 	cat >>Makefile <<EOF
 cjk/$italic: cjk/$file
-	python italize.py cjk/$file cjk/$italic 0.1733
+	PYTHONPATH=.. python italize.py cjk/$file cjk/$italic 0.1733
 EOF
 done
 for file in NotoSerifCJKsc-{Regular,Bold}.otd; do
 	italic=$(echo $file | sed 's/.otd/Italic.otd/;s/Regular//')
 	cat >>Makefile <<EOF
 cjk/$italic: cjk/$file
-	python italize.py cjk/$file cjk/$italic 0.2125
+	PYTHONPATH=.. python italize.py cjk/$file cjk/$italic 0.2125
 EOF
 done
 
@@ -80,16 +80,16 @@ for file in NotoSansCJKsc-{Light,Regular,Medium,Bold}.otd; do
 	condensed=${file/-/Condensed-}
 	cat >>Makefile <<EOF
 cjk/$condensed: cjk/$file
-	python condense.py cjk/$file cjk/$condensed
+	PYTHONPATH=.. python condense.py cjk/$file cjk/$condensed
 EOF
 done
 
 # phase 1: convert Noto CJK OpenType/CFF to OpenType/TT
-for basename in NotoSansCJK{jp,kr,sc,tc}-{Thin,Light,Regular,Medium,Bold,Black} NotoSerifCJKsc-{Regular,Bold}; do
+for basename in NotoSansCJKsc-{Thin,Light,Regular,Medium,Bold,Black} NotoSerifCJKsc-{Regular,Bold}; do
 	cat >>Makefile <<EOF
-cjk/$basename.otd: src/$basename.otf
+cjk/$basename.otd: ../src/$basename.otf
 	mkdir -p cjk/
-	otfccdump --ignore-hints src/$basename.otf | otfcc-c2q | otfccbuild -o cjk/$basename.ttf
+	otfccdump --ignore-hints ../src/$basename.otf | otfcc-c2q | otfccbuild -o cjk/$basename.ttf
 	otfccdump cjk/$basename.ttf -o cjk/$basename.otd
 EOF
 done
